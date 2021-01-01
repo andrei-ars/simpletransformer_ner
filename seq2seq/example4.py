bcc2efd0ccb61d97757ed3532ad957704f4122c7
Andrei <z@sp.sp>
2021-01-01 17:03:35 +0300

    1

--- seq2seq/example4.py
+++ seq2seq/example4.py
@@ -129,10 +129,8 @@ model_args.evaluate_during_training_verbose = True
 
 # Initialize model
 model = Seq2SeqModel(
-    encoder_decoder_type="distilbert",
-    encoder_decoder_name="distilbert-base-uncased-distilled-squad",
-    #encoder_decoder_type="bart",
-    #encoder_decoder_name="facebook/bart-large",
+    encoder_decoder_type="bart",
+    encoder_decoder_name="facebook/bart-large",
     #encoder_decoder_name="facebook/bart-base",
     args=model_args,
     use_cuda=False,
