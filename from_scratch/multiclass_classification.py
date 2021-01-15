30ecbf7fd21ada6b36dbede5b01d0bed531c8547
Andrei <z@sp.sp>
2021-01-15 09:21:12 +0300

    1

--- from_scratch/multiclass_classification.py
+++ from_scratch/multiclass_classification.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
             num_labels=num_labels,
             args={"reprocess_input_data": True, 
                     "overwrite_output_dir": True,
-                    'num_train_epochs': 5,   # 5
+                    'num_train_epochs': 2,   # 5
                     'train_batch_size': 32,
                  },
             use_cuda=False
