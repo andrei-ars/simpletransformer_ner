360a6c97b6f39f9a508a8a579cfc78555c7cb30a
Andrei <z@sp.sp>
2021-02-17 22:17:49 +0300

    1

--- ner_lm/train_complex_dataset.py
+++ ner_lm/train_complex_dataset.py
@@ -76,6 +76,7 @@ if __name__ == "__main__":
         #    text = test_sentences[i]
         #    print("text: {}\noutput: {}\n".format(text, predictions[i]))
 
+        model.eval()
         print("\nMODEL.TEST:")
         model.test()
 
