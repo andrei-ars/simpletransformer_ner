3a99660936263186ded30cc9422711ca8b2eb17c
Andrei <z@sp.sp>
2021-02-22 23:51:48 +0300

    1

--- lm_train/train_new_lm.py
+++ lm_train/train_new_lm.py
@@ -52,7 +52,9 @@ test_file = f"data/test.txt"
 
 model_args = LanguageModelingArgs()
 model_args.config = {
-    "num_hidden_layers": 2
+    "hidden_size": 768,
+    "num_hidden_layers": 6,  # 12
+    "num_attention_heads": 6,
 }
 model_args.vocab_size = 10000
 model_args.output_dir = "../{}/from_scratch_".format(output_dir_name)
