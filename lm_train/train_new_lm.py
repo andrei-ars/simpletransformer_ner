1bf0eecca34a6b5ed3056ca914dc533f4d621b43
Andrei <z@sp.sp>
2021-03-01 01:43:44 +0300

    change paths

--- lm_train/train_new_lm.py
+++ lm_train/train_new_lm.py
@@ -58,8 +58,8 @@ test_file = "{}/data/test.txt".format(this_folder)
 model_args = LanguageModelingArgs()
 model_args.config = {
     "hidden_size": 768,
-    "num_hidden_layers": 6,  # 12
-    "num_attention_heads": 6,
+    "num_hidden_layers": 2,  # 12
+    "num_attention_heads": 2,
 }
 model_args.vocab_size = 10000
 model_args.output_dir = "{}/from_scratch_".format(output_dir_name)
