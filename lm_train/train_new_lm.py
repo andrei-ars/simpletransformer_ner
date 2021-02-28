fca4b708db16b062805bb438c062674d20ecfa24
Andrei <z@sp.sp>
2021-03-01 01:04:31 +0300

    change paths

--- lm_train/train_new_lm.py
+++ lm_train/train_new_lm.py
@@ -64,7 +64,7 @@ model_args.config = {
 model_args.vocab_size = 10000
 model_args.output_dir = "../{}/from_scratch_".format(output_dir_name)
 model_args.best_model_dir = "../{}/from_scratch/best_model".format(output_dir_name)
-model_args.num_train_epochs = 3
+model_args.num_train_epochs = 1
 model_args.save_eval_checkpoints = True
 model_args.overwrite_output_dir = True
 
--- train_ner_from_scratch.sh
+++ train_ner_from_scratch.sh
@@ -1,2 +1,2 @@
 rm lm_outputs_test/* -rf
-python ./lm_train/train_new_lm.py
+python lm_train/train_new_lm.py
