9310d82fb17bf85c6e32524e11f6c8e0f065552f
Andrei <z@sp.sp>
2021-03-02 01:19:20 +0300

    change paths

--- ner_lm/nermodel.py
+++ ner_lm/nermodel.py
@@ -51,7 +51,7 @@ class NerModel:
 
         #self.model = NERModel("bert", pretrained_model_name, use_cuda=False, args=model_args)
         #self.model = NERModel("bert", "bert-base-uncased", use_cuda=False, args=model_args)
-        self.model = NERModel("albert", "albert-base-uncased", use_cuda=False, args=model_args)
+        self.model = NERModel("longformer", "allenai/longformer-base-4096", use_cuda=False, args=model_args)
         #self.model = NERModel("electra", 'google/electra-small-generator', use_cuda=False, args=model_args)
         #self.model = NERModel("layoutlm", 'microsoft/layoutlm-base-uncased', use_cuda=False, args=model_args)
 
