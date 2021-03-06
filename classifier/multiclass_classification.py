70b4634a21414bc2ad417771e0f100f425f6879f
Andrei <z@sp.sp>
2021-03-06 21:59:11 +0300

    1

--- classifier/multiclass_classification.py
+++ classifier/multiclass_classification.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
             args={"reprocess_input_data": True, 
                     "overwrite_output_dir": True,
                     'num_train_epochs': 2,   # 5
-                    'train_batch_size': 12,  # 32 for bert (but 32 gives an error for longfomer)
+                    'train_batch_size': 10,  # 32 for bert (but >10 gives an error for longfomer)
                  },
             use_cuda=False
         )        
