2515ab8e7e8dd90e57329607284547deef0c112d
Andrei <z@sp.sp>
2021-03-29 02:05:53 +0300

    1

--- ner_lm/train_complex_dataset.py
+++ ner_lm/train_complex_dataset.py
@@ -10,7 +10,7 @@ from ner_slot_filling import split_token_tag, ner_slot_filling
 
 if __name__ == "__main__":
 
-    #mode = "train"
+    mode = "train"
     #mode = "test"
     #mode = "infer"
     pretrained_type = "English"
