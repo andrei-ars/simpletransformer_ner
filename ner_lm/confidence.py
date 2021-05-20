136e211c0e20bd504d3b1896d33a04443616b109
Andrei <z@sp.sp>
2021-05-20 03:44:44 +0300

    1

---
+++ ner_lm/confidence.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from mathfunctions import softmax_probabilities
+
+
+def calc_confidence(raw_outputs, labels_list=None):
+
+    result = raw_outputs[0]
+    print(result)
+    probs = []
+    for dc in result:
+        for key in dc:
+            print('\nTOKEN:', key)
+            for logit in dc[key]:
+            #logit = dc[key][0]
+                print(key, len(logit))
+                ps = softmax_probabilities(logit)
+                print('max_ps:', max(ps))
+                probs.append(ps)
+                index = np.argmax(ps)
+                print('index:', index)
+                if labels_list:
+                    print('tag:', labels_list[index])
+
+    return min(probs)
+
+
+if __name__ == "__main__":
+    raw_outputs = [[{'Click': [[0.9166969, 7.8369393, 0.31039014, -0.60283166, -1.2205212, -1.0528294, -0.57920927, -1.8390691, -0.7053572, 0.72872484, -1.2057197, -1.1906811, -0.2462096, -0.73678666]]}, {'on': [[2.864785, 0.81569016, -2.4111693, -1.753845, -1.3591471, -0.45600742, 0.22175379, -0.5122926, -0.43606478, 6.4333878, -1.3908641, -0.48224172, -0.9318897, -1.1649382]]}, {'Basket': [[3.537952, -0.11874729, -0.6143042, 4.150231, -1.6462238, 0.49634176, -1.2271496, -1.7730664, 0.10150815, 0.36229157, -1.1427803, 1.7072755, -0.07331692, -0.8622093], [0.2326435, -0.2758199, -0.67160505, 5.107534, -1.1315546, -0.060734212, -1.6606357, -0.8778804, -0.74439603, -0.7747257, -1.1741363, 3.4446402, -0.66995436, -0.360082]]}]]
+    labels_list = ['O', 'B-ACT', 'B-CNT', 'B-OBJ', 'B-OPE', 'B-ORD', 'B-PRE', 'B-TYP', 'B-VAL', 'I-ACT', 'I-CNT', 'I-OBJ', 'I-OPE', 'I-PRE']
+    #print(raw_outputs)
+
+    calc_confidence(raw_outputs, labels_list)
