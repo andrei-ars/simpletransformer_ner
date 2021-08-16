a74ff72a322fb1cf711e5844f72e173176af2f7c
Andrei <z@sp.sp>
2021-08-16 03:04:40 +0300

    update

--- ner_lm/train_complex_dataset.py
+++ ner_lm/train_complex_dataset.py
@@ -11,9 +11,9 @@ from ner_slot_filling import ner_slot_filling #, ner_slot_filling_compound
 
 if __name__ == "__main__":
 
-    #mode = "train"
+    mode = "train"
     #mode = "test"
-    mode = "infer"
+    #mode = "infer"
     #pretrained_type = "English"
     #pretrained_type = "LM"
     #pretrained_type = "continue"
